ca336ff14a954532372a7d673110764f4507a86f
Brian <briansmith@rocketcentral.com>
2022-11-07 16:59:09 -0500

    This is working

--- graphql-client/src/app/app-routing.module.ts
+++ graphql-client/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BrowseComponent } from './browse/browse.component';
+import { CharacterComponent } from './character/character.component';
+import { SpeciesComponent } from './species/species.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'browse',
+    component: BrowseComponent,
+  },
+  {
+    path: 'character',
+    component: CharacterComponent,
+  },
+  {
+    path: 'species',
+    component: SpeciesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
