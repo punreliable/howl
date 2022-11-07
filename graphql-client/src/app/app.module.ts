1d08d2eeb429b33c3925a3cca1195b0c1731ff7c
Brian <briansmith@rocketcentral.com>
2022-11-07 15:28:34 -0500

    Added three components

--- graphql-client/src/app/app.module.ts
+++ graphql-client/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BrowseComponent } from './browse/browse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule,
